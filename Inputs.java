695c81ddcda6458d808afd628c2731299a292c5b
kartikjsonawane <kartikjaywantsonawane@gmail.com>
2026-02-15 13:19:51 +0000

    created file

---
+++ Inputs.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public  class Inputs {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        int rno=i.nextInt();
+        System.out.println(rno);
+    }
+}
