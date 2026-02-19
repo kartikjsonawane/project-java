0e09a8e3646967d22c52b345a10312bad277d55b
kartikjsonawane <kartikjaywantsonawane@gmail.com>
2026-02-19 06:26:19 +0000

    loops

--- Conditonal.java
+++ Conditonal.java
@@ -1,6 +1,6 @@
 public class Conditonal {
     public static void main(String[] args) {
-        int sal=25000;
+        /*int sal=25000;
         if(sal>10000){
             sal=sal+1000;
         }
@@ -10,6 +10,27 @@ public class Conditonal {
         else{
             sal+=3000;
         }
-        System.out.println(sal);
+        System.out.println(sal);*/
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+"*"+" ");
+            }
+            System.out.println(" ");
+        }
+        int i=5;
+        while(i>=1){
+            int j=1;
+            while(j<=i){
+                j++;
+                System.out.print("*");
+            }
+            i--;
+            System.out.println(" ");
+        }
+        int k=5;
+        do{
+            System.out.println(k);
+            k--;
+        }while(k>1);
     }
 }
