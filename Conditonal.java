895773169aee359168203545420c63c866b3807d
kartikjsonawane <kartikjaywantsonawane@gmail.com>
2026-02-19 06:08:14 +0000

    if-statment

---
+++ Conditonal.java
@@ -0,0 +1,12 @@
+public class Conditonal {
+    public static void main(String[] args) {
+        int sal=25000;
+        if(sal>10000){
+            sal=sal+1000;
+        }
+        else{
+            sal=sal+2000;
+        }
+        System.out.println(sal);
+    }
+}
