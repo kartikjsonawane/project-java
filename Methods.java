f2ba0af35e7516e66ac518246b9260a249520fe5
kartikjsonawane <kartikjaywantsonawane@gmail.com>
2026-02-19 07:16:21 +0000

    methods creation

---
+++ Methods.java
@@ -0,0 +1,10 @@
+class Methods{
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Methods obj =new Methods();
+        System.out.println(add(3,5));
+    }
+}
